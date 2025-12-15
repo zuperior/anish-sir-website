b4b93ee43801a81eaa7603cf2b5226309ea9892e
Niranjan Subhedar <niranjansubhedar@gmail.com>
2025-12-15 17:22:24 +0530

    enhance layout and responsiveness of BookGrid and HeroSection components

--- components/resources/BookGrid.tsx
+++ components/resources/BookGrid.tsx
@@ -55,12 +55,12 @@ const books = [
 
 const BookGrid = () => {
   return (
-    <div className="bg-[#151515] w-full min-h-screen p-[100px]">
-      <div className="grid  md:grid-cols-4 grid-cols-1 lg:grid-cols-4 justify-items-center gap-16 w-fit mx-auto">
+    <div className="bg-[#151515] w-full min-h-screen md:p-[90px] p-[40px] lg:p-[100px] ">
+      <div className="grid  md:grid-cols-4 grid-cols-2 lg:grid-cols-4 justify-items-center gap-16 w-fit mx-auto">
         {books.map((book) => (
           <div
             key={book.title}
-            className="flex flex-col items-start justify-between gap-4 w-[190px]"
+            className="flex flex-col items-start justify-between gap-4 md:w-[190px] w-[160px] lg:w-[190px]"
           >
             <div className="space-y-4">
               {/* Book Image */}
@@ -69,14 +69,15 @@ const BookGrid = () => {
                 alt={book.title}
                 width={190}
                 height={279}
+                className="lg:w-[190px] lg:h-[279px] h-[220px] w-[150px]"
               />
 
               {/* Title + Author */}
               <div className="text-left space-y-1">
-                <h3 className="font-medium text-white text-lg leading-tight">
+                <h3 className="font-medium text-white lg:text-lg md:text-lg text-[15px] leading-tight">
                   {book.title}
                 </h3>
-                <p className="font-clash-grotesk text-base text-white/70">
+                <p className="font-clash-grotesk lg:text-base md:text-base text-[13px] text-white/70">
                   {book.author}
                 </p>
               </div>
@@ -87,7 +88,7 @@ const BookGrid = () => {
               href={book.link}
               target="_blank"
               rel="noopener noreferrer"
-              className="flex items-center border border-gray-700 rounded-full px-4 py-2 text-white hover:bg-white/10 transition tracking-tight font-clash-grotesk font-medium text-[15px] w-full justify-center"
+              className="flex items-center border border-gray-700 rounded-full md:px-4 px-2 lg:px-4 py-2 text-white hover:bg-white/10 transition tracking-tight font-clash-grotesk font-medium text-[15px] w-full justify-center"
             >
               Buy from Amazon
               <Image
